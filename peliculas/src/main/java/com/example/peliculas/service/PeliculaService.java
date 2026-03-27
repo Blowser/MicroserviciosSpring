d475165d88bf7386f8d464526865f27405d04e13
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-03-27 20:54:45 -0300

    Create PeliculaService.java

---
+++ peliculas/src/main/java/com/example/peliculas/service/PeliculaService.java
@@ -0,0 +1,38 @@
+package com.example.peliculas.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.peliculas.dto.PeliculaDTO;
+
+@Service
+public class PeliculaService {
+
+    private List<PeliculaDTO> peliculas = new ArrayList<>();
+
+    public PeliculaService() {
+        peliculas.add(new PeliculaDTO("1", "Shrek", "Andrew Adamson", "Animación"));
+        peliculas.add(new PeliculaDTO("2", "Interestelar", "Christopher Nolan", "Ciencia Ficción"));
+        peliculas.add(new PeliculaDTO("3", "El Padrino", "Francis Ford Coppola", "Drama"));
+    }
+
+    public List<PeliculaDTO> obtenerTodos() {
+        return peliculas;
+    }
+
+    public PeliculaDTO obtenerPorId(String id) {
+        for (PeliculaDTO p : peliculas) {
+            if (p.getId().equals(id)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public PeliculaDTO crear(PeliculaDTO nueva) {
+        peliculas.add(nueva);
+        return nueva;
+    }
+}
