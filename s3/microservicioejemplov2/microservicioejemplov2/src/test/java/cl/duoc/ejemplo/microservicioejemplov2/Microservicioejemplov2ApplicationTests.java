6223429529dbe7da409670959c928c1765a12bd1
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-03-28 20:23:59 -0300

    Create Microservicioejemplov2ApplicationTests.java

---
+++ s3/microservicioejemplov2/microservicioejemplov2/src/test/java/cl/duoc/ejemplo/microservicioejemplov2/Microservicioejemplov2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ejemplo.microservicioejemplov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicioejemplov2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
