54a83d33bc71b0304de290e828647c261d31b551
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-03-29 16:37:03 -0300

    Update ProductoMascotaDTO.java
    ACTIVACIÓN DE VALIDACIONES

--- mascotas/src/main/java/com/semana3/mascotas/dto/ProductoMascotaDTO.java
+++ mascotas/src/main/java/com/semana3/mascotas/dto/ProductoMascotaDTO.java
@@ -1,18 +1,24 @@
 package com.semana3.mascotas.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class ProductoMascotaDTO {
 
     private String id;
+
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+
+    @NotBlank(message = "La categoría es obligatoria")
     private String categoria;
+
+    @Positive(message = "El precio debe ser positivo")
     private int precio;
 }
