aa6510cfa71d57fe9a3e2ee7d71e0a55e39a7f70
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-04-17 18:54:44 -0400

    Create BdmoviesApplication.java

---
+++ s4/bdmovies/src/main/java/com/example/bdmovies/BdmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdmoviesApplication.class, args);
+	}
+
+}
