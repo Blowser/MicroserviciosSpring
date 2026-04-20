cf21343be1aecb90755804cd5101bc6013141b1d
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-04-20 18:48:12 -0400

    Create BdmoviesResponseDTO.java

---
+++ s5/bdmovies/src/main/java/com/example/bdmovies/dto/BdmoviesResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.bdmovies.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BdmoviesResponseDTO {
+    private Long id;
+    private String titulo;
+    private String director;
+    private String genero;
+    private Double rating;
+}
+
