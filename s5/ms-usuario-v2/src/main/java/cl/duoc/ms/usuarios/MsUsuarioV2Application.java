bee05809e50ed890772cbf126cc18b2ba2c18dd6
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-04-20 18:50:15 -0400

    Create MsUsuarioV2Application.java

---
+++ s5/ms-usuario-v2/src/main/java/cl/duoc/ms/usuarios/MsUsuarioV2Application.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuarioV2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MsUsuarioV2Application.class, args);
+	}
+}
