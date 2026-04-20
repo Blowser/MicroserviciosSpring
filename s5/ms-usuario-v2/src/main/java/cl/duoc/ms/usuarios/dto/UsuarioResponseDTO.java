76627abbb859ee17b69cc78964abd84c7ced69a4
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-04-20 18:49:59 -0400

    Create UsuarioResponseDTO.java

---
+++ s5/ms-usuario-v2/src/main/java/cl/duoc/ms/usuarios/dto/UsuarioResponseDTO.java
@@ -0,0 +1,19 @@
+package cl.duoc.ms.usuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioResponseDTO {
+
+	private Long id;
+
+	private String nombre;
+
+	private String email;
+
+	private Integer edad;
+}
