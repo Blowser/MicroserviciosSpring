ab68f81819fd5fac395cf967acb8b867cbc03e23
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-04-20 18:48:42 -0400

    Create BdmoviesApplicationTests.java

---
+++ s5/bdmovies/src/test/java/com/example/bdmovies/BdmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
